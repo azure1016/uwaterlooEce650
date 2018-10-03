f536cecd3121059879e177cba9c0040af3bb2c85
Wangdan <wdazure1016@163.com>
2018-10-03 10:19:44 -0400

    final commit from local

--- a1ece650.py
+++ a1ece650.py
@@ -1,19 +1,12 @@
-#!/usr/bin/python
-# -*- coding: <utf-8> -*-
-from __future__ import division
-import sys
-#import matplotlib
-import re
-#import numpy as np
-#import pylab as pl
-#from matplotlib import collections  as mc
-import intersect
 
-#you can use py3to2 to change python3 to python2
+from __future__ import division
+import re
+import sys
+
+# YOUR CODE GOES HERE
 class Street(object):
     vb_dc_l = {} 
     #{
-
     #"st2":[Point(3,4), Point(4,5)]
     #}
 
@@ -36,26 +29,28 @@ class Street(object):
 
     def add_street(self,street_name,vi_l,ei_l):
         #vi_l = get_coordinates(),ei_l = get_lines()
-        if self.vb_dc_l.has_key(street_name.upper()) == False:
-            self.vb_dc_l[street_name.upper()] = vi_l
-            self.eb_dc_l[street_name.upper()] = ei_l
+        stn = street_name.upper()
+        if self.vb_dc_l.has_key(stn) == False:
+            self.vb_dc_l[stn] = vi_l
+            self.eb_dc_l[stn] = ei_l
             #self.edges_list.add(ei_l)
         else:
-            sys.stdout("You added a street that has existed!")
+            sys.stderr.write("Warning: invalid command: 'a' specified a street that has already existed \n")
+            #return
 
     def delete_street(self,street_name):
-        if self.vb_dc_l.has_key(street_name.upper()) == False:
+        if self.vb_dc_l.has_key(street_name.upper()) == True:
             del self.vb_dc_l[street_name.upper()]
             del self.eb_dc_l[street_name.upper()]
         else:
-            sys.stdout("The street you want to delete doesn't even exist!")
+            sys.stderr.write("Warning: invalid command:The street you want to delete doesn't even exist!\n")
 
     def change_street(self,street_name,newvi_l,newei_l):
-        if self.vb_dc_l.has_key(street_name.upper()) == False:
+        if self.vb_dc_l.has_key(street_name.upper()) == True:
             self.vb_dc_l[street_name.upper()] = newvi_l
             self.eb_dc_l[street_name.upper()] = newei_l
         else:
-            sys.stdout("The street you want to change doesn't exist. Please check again.")
+            sys.stderr.write("Warning: invalid command:The street you want to change doesn't exist. Please check again.\n")
 
     # def show_graph(self):
     #     if len(self.vb_dc_l)>0:
@@ -74,63 +69,67 @@ class Street(object):
 
     def text_graph(self):
         self.process_graph()
-        sys.stdout.write("V = {")
+        self.valid_points.sort(key=lambda pt: pt.index,reverse = False)
+        sys.stdout.write("V = {\n")
         for v in self.valid_points:
-            sys.stdout.write(v+"\n")
-        sys.stdout.write("}")
-        sys.stdout.write("E = {")
-        for e in self.valid_edges:
-            sys.stdout.write(e+"\n")
-        sys.stdout.write("}")
+            sys.stdout.write(str(v)+"\n")
+        sys.stdout.write("}\n")
+        sys.stdout.write("E = {\n")
+        for i in range(0,len(self.valid_edges)-1):
+            sys.stdout.write(str(self.valid_edges[i])+",\n")
+        if len(self.valid_edges)>0:
+            sys.stdout.write(str(self.valid_edges[-1])+'\n')
+        sys.stdout.write("}\n")
 
     # def flags_clear(self):
     #     self.flags = [0,0,0]
     
     def get_coordinates(self,str_read):
         # return {"A":(a,b),"B":(c,d)}
-        cord =  re.findall(r'(\(\s*\d+\s*,\s*\d+\s*\)\s*)',str_read) 
-        #try:
+        #better use "match" rather than "findall"!!
+        cord =  re.findall(r'(\(\s*[-+]?[0-9]*\.?[0-9]+\s*,\s*[-+]?[0-9]*\.?[0-9]+\s*\)\s*)',str_read)
+        try:
             #might have invalid input so that can't eval
-        vertices = []
-        for val in cord:
-            cor = eval(val)
-            p = Point(cor[0],cor[1],self.uniq_v_index)
-            self.point_history.append(p)
-            vertices.append(p)
-            self.uniq_v_index = self.uniq_v_index + 1
-        return vertices
-        #except exception1:
-            #pass# for now
-
-    # def get_edges(self):
-    #     #return a list of Line objects
-    #     v_k = vi_l.keys()
-    #     edges = []
-    #     for i in range(0,len(v_k)-2):
-    #         edges[i] = (i,i+1)
-    #     return edges
+            vertices = []
+            for val in cord:
+                cor = eval(val)
+                p = Point(cor[0],cor[1],self.uniq_v_index)
+                if not p.is_in(self.point_history):  # a brand new
+                    self.uniq_v_index = self.uniq_v_index + 1
+                    self.point_history.append(p)
+                else:  # not a brand new
+                    for ph in self.point_history:
+                        if p == ph:
+                            p.index = ph.index
+                            break
+                vertices.append(p)
+            return vertices
+        except:
+            sys.stderr.write("Warning: invalid coordinates")
+            return None
 
     def get_lines(self,vi_l):
         #vi_l is a dictionary. return a list:[(1,2),(2,3),(3,4)]
-        for k, v in self.vb_dc_l.items():
-            for i in range(0,len(v)-2):
-                l = Line(v[i],v[i+1])
-                self.eb_dc_l[k].append(l)
+        ei_l = []
+        rg = range(0,len(vi_l)-1)
+        for i in rg:
+            l = Line(vi_l[i],vi_l[i+1])
+            ei_l.append(l)
+        return ei_l
 
     def process_cmd(self,str_read):
         #cmd = re.search(r'^(\s)*[ac]\s+"((\w+\s*)+)"\s+(\(.*?\)\s*)+',str_read)
-        #self.flags_clear()
-        cmd_ac = re.search(r'^\s*([ac])\s+".*?"\s+(\(.*?\)\s*)+$',str_read)
-        if cmd_ac != None:
+        cmd_ac = re.search(r'\s*([ac])\s+"([a-zA-Z\s]+)"\s+(\(\s*[-+]?[0-9]*\.?[0-9]+\s*,\s*[-+]?[0-9]*\.?[0-9]+\s*\)\s*)+',str_read)
+        if cmd_ac != None and cmd_ac.group(0) == str_read:
             vi_l = self.get_coordinates(str_read)
-            ei_l = self.get_lines(str_read)
+            ei_l = self.get_lines(vi_l)
             str_list = cmd_ac.groups()
             if str_list[0] == "a":               
                 self.add_street(str_list[1],vi_l,ei_l)
             elif str_list[0] == "c":
                 self.change_street(str_list[1],vi_l,ei_l)
         else:
-            cmd_r = re.search(r'^(\s)*[r]\s+"(.*?)"\s*$',str_read) 
+            cmd_r = re.search(r'^\s*[r]\s+"(.*?)"\s*$',str_read)
             if cmd_r != None:
                 #street_name = re.sub(r'\"\s+',"\"",cmd_r.group(2))
                 #street_name = 
@@ -141,7 +140,9 @@ class Street(object):
                 if cmd_g != None:
                     self.text_graph()
                     #return True
-        #return False
+                else:
+                    sys.stderr.write("Invalid command: no match for the line. \n")
+
     def add_if_not_in(self,some_list,some_storage):
         for i in some_list:
             if not i.is_in(some_storage):
@@ -149,37 +150,42 @@ class Street(object):
 
     def process_graph(self):
         #process valid_points
+        del self.valid_edges[:]
+        del self.valid_points[:]
+        for l_o in self.eb_dc_l.keys():
+            for l_n in self.eb_dc_l[l_o]:
+                del l_n.intersections[:]
         for i in self.eb_dc_l.keys():
             for j in self.eb_dc_l.keys():
                 if i!=j:
                     for l1 in self.eb_dc_l[i]:
                         for l2 in self.eb_dc_l[j]:
-                            p_l = self.intersect_on_segment(l1,l2,self.uniq_v_index)
+                            p_l = intersect_on_segment(l1,l2,self.uniq_v_index)
                             #when no valid point found,return a none object rather than a list?
                             if p_l != None:
                                 for p in p_l:
                                     if not p.is_in(self.point_history):# a brand new
                                         self.uniq_v_index = self.uniq_v_index + 1
                                         self.point_history.append(p)
-                                        self.valid_points.append(p)
-                                        self.add_if_not_in([p],self.valid_points)
-                                        # if not p.is_in(l1.intersections):
-                                        self.add_if_not_in([p],l1.intersections)
-                                        #if not p.is_in(l2.intersections):
-                                        self.add_if_not_in([p],l2.intersections)
                                     else:#not a brand new, but may not previously in line.intersections
                                         for ph in self.point_history:
                                             if p==ph:
                                                 p.index = ph.index
                                                 break
-                                        self.add_if_not_in([p,l1.src,l1.dst,l2.src,l2.dst],self.valid_points)
-                                        self.add_if_not_in([p],l1.intersections)
-                                        self.add_if_not_in([p],l2.intersections)
+                                    self.add_if_not_in([p,l1.src,l1.dst,l2.src,l2.dst],self.valid_points)
+                                    self.add_if_not_in([p],l1.intersections)
+                                    self.add_if_not_in([p],l2.intersections)
                                 #if p_l!=None, then you can expect valid edges
-                                v_p = [l1.src,l1.dst].append(l1.intersections)
-                                v_p.sort()
-                                for i in range(0,len(v_p)-2):
-                                    self.valid_edges.append(Line(v_p[i],v_p[i+1]))
+        for i_i in self.eb_dc_l.keys():
+            for l_i in self.eb_dc_l[i_i]:
+                if len(l_i.intersections)>0: #at this time, l2 has been compared with all the lines
+                    v_p2 = [l_i.src, l_i.dst]
+                    v_pn2 = list(set(v_p2+l_i.intersections))
+                    v_pn2.sort(key=lambda pt: pt.x, reverse = False)
+                    v_pn2.sort(key=lambda pt: pt.y, reverse = False)
+                    for ix in range(0, len(v_pn2) - 1):
+                        if not v_pn2[ix].__eq__(v_pn2[ix+1]):
+                            self.add_if_not_in([Line(v_pn2[ix], v_pn2[ix + 1])],self.valid_edges)
     
 class Point(object):
     def __init__ (self, x, y, index):
@@ -197,23 +203,41 @@ class Point(object):
 
     def __le__ (self, other):
         return self.x <= other.x
-    
+
+    def is_in(self,some_storage):
+        for i in some_storage:
+            if i.x == self.x and i.y == self.y:
+                return True
+            else:
+                continue
+        return False
+
 class Line(object):
-    intersections = []
     def __init__ (self, src, dst):
         self.src = src
         self.dst = dst
+        self.intersections = []
 
     def __str__ (self):
         return "<"+str(self.src.index) + ',' + str(self.dst.index)+">"
 
     def __ge__ (self,other):
-        ge1 = self.src < other.src and self.src < other.dst and self.dst > other.src and self.dst > other.dst
-        ge2 = self.dst < other.src and self.dst < other.dst and self.src > other.src and self.dst > other.dst
+        ge1 = self.src <= other.src and self.src <= other.dst and self.dst >= other.src and self.dst >= other.dst
+        ge2 = self.dst <= other.src and self.dst <= other.dst and self.src >= other.src and self.dst >= other.dst
         return any([ge1, ge2])
 
     def __eq__ (self,other):
         return self.src == other.src and self.dst == other.src
+
+    def is_in(self,some_storage):
+        for i in some_storage:
+            g1 = i.src.index == self.src.index and i.dst.index == self.dst.index
+            g2 = i.dst.index == self.src.index and i.src.index == self.dst.index
+            if any([g1,g2]) == True:
+                return True
+            else:
+                continue
+        return False
     
 def intersect_on_segment (l1, l2,new_index):
     x1, y1 = l1.src.x, l1.src.y
@@ -229,9 +253,9 @@ def intersect_on_segment (l1, l2,new_index):
     if denominator == 0: 
         #coincident
         if (x1-x2) * (y2-y3) == (x2-x3) * (y1-y2):
-            if l1 >= l2:
+            if l1.__ge__(l2):
                 return [l2.src,l2.dst]
-            elif l2 >= l1:
+            elif l2.__ge__(l1):
                 return [l1.src,l1.dst]
             else:
                 return None
@@ -253,41 +277,24 @@ def intersect_on_segment (l1, l2,new_index):
         else:
             return None
 
-# class VertexCover(object):
-#     valid_vertices = {}
-#     valid_edges = {
-#     #"street_1":[(a,b),(b,c)],
-#     #"street_2":[],
-#     #"street_3":[]...
-#     }
 
-#     def __init__(self,v_dc,e_l):
-#         valid_vertices = v_dc
-#         valid_edges = e_l
+def main():
+    ### YOUR MAIN CODE GOES HERE
+    ns = Street()
+    sys.stdout.write("please input the command:")
+    ### sample code to read from stdin.
+    ### make sure to remove all spurious print statements as required
+    ### by the assignment
+    while True:
+        line = sys.stdin.readline()
+        if line == '':
+            break
+        print 'read a line:', line
+        ns.process_cmd(line)
 
-#     def make_valid_cover(self,v_dc,e_l):
-#         #e_l have to be a list rather than a dictionary
-#         #e_l = Street().eb_dc_l.values(), a list of list
-#         e_end = len(self.e_l) -1 #the total streets
-#         for i in range(0,e_end):
-#             st_i = self.e_l[i]
-#             for j in range(i+1,e_end):
-#                 st_j = self.eb_dc_l[j]
-#                 for m in range(0,len(st_i)-1):
-#                     e1 = st_i[m]#edges on the street
-#                     for n in range(0,len(st_j)-1):
-#                         e2 = st_j[n]
-#                         e1_seg = Line(Point(e1[0],e1[1]),Point())
-                        
-# try:
-ns = Street()
-sys.stdout.write("please input the command:")
-while True:
-    std_read = sys.stdin.readline()#[:-1] #discard '\n' at the end
-    #I heard that stdin will get a \n
-    #std_read = a "King Street" (2,1) (1.2,2.3) (4.2,2)
-    #if ns.isValidCmd(std_read):
-    ns.process_cmd(std_read)
+    print 'Finished reading input'
+    # return exit code 0 on successful termination
+    sys.exit(0)
 
-
-
+if __name__ == "__main__":
+    main()
